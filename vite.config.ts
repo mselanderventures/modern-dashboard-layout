e8be312aa812ba27e0afd920320e5c4a180decf1
Michael Selander <selanderventures@gmail.com>
2025-02-05 17:47:02 +0000

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: '/modern-dashboard-layout/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
